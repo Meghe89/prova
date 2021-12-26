66689ae51a24617172ad425e8c8e4c687955cfc7
Michele Cozzola <cozzolamichele89@gmail.com>
2021-12-26 13:09:25 +0100

    Days js formula

---
+++ IconAndCard/script.js
@@ -0,0 +1,55 @@
+let day = document.querySelector('#day');
+let monthName = document.querySelector('#month');
+
+let today = new Date();
+let date = today.getDate()
+let month =  today.getMonth()+1
+
+
+function nameMonth(month) {
+    switch (month) {
+        case 1:
+            return 'Jan';
+            break;    
+        case 2:
+            return 'Feb';
+            break;    
+        case 3:
+            return 'Mar';
+            break;    
+        case 4:
+            return 'Apr';
+            break;    
+        case 5:
+            return 'May';
+            break;    
+        case 6:
+            return 'Jun';
+            break;    
+        case 7:
+            return 'Jul';
+            break;    
+        case 8:
+            return 'Aug';
+            break;    
+        case 9:
+            return 'Sep';
+            break;    
+        case 10:
+            return 'Oct';
+            break;    
+        case 11:
+            return 'Nov';
+            break;    
+        case 12:
+            return 'Dec';
+            break;    
+        default:
+            break;
+    }
+}
+
+day.innerHTML = date;
+monthName.innerHTML = nameMonth(month);
+ 
+
